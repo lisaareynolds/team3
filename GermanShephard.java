e8c8d59aeb2393e35821fe46b962ead064bcc630
dkra3744 <dnlkrlvtz@gmail.com>
2017-05-23 08:27:16 -0600

    Update GermanShephard.java

--- GermanShephard.java
+++ GermanShephard.java
@@ -1,5 +1,6 @@
 /**
  * Created by cjoh7720 on 5/15/2017.
  */
-public class GermanShephard {
+public class GermanShephard extends Animal implements Dog {
+  public String gerShepCharacteristics = "";
 }
