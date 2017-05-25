afe5132d68fe8e2d41b2e1f7530c4aac0fbdbea4
NickCollins006 <NickCollins006@gmail.com>
2017-05-25 08:10:04 -0600

    Update Pug.java

--- Pug.java
+++ Pug.java
@@ -2,5 +2,5 @@
  * Created by cjoh7720 on 5/15/2017.
  */
 public class Pug extends Animal implements Dog {
-    public String pugCharacteristics = "";
+    private String pugCharacteristics = "Breed of dog with a wrinkly, short-muzzled face, and curled tail.";
 }
