e4f0c9361c543ca651e66eb4ca9e60e8e633b350
NickCollins006 <NickCollins006@gmail.com>
2017-05-25 08:27:15 -0600

    Update Siamese.java

--- Siamese.java
+++ Siamese.java
@@ -1,6 +1,6 @@
 /**
  * Created by cjoh7720 on 5/15/2017.
  */
-public class Siamese extends Animal {
+public class Siamese extends Animal implements Cat {
   private String siameseCharacteristics = "Medium-sized cat with spottings very similar to a wild linx.";
 }
