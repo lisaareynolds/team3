40beaaa0a363eead583146325e0924a1c4e3a1ea
NickCollins006 <NickCollins006@gmail.com>
2017-05-25 08:11:27 -0600

    Update Ragdoll.java

--- Ragdoll.java
+++ Ragdoll.java
@@ -2,4 +2,5 @@
  * Created by cjoh7720 on 5/15/2017.
  */
 public class Ragdoll extends Animal {
+  private String ragdollCharacteristics = " Large and muscular semi-longhair cat with a soft and silky coat.";
 }
