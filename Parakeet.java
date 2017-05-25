3cb28dc28c2ed429e8ffbcbc82506bd4a9546c3d
NickCollins006 <NickCollins006@gmail.com>
2017-05-25 08:30:05 -0600

    Update Parakeet.java

--- Parakeet.java
+++ Parakeet.java
@@ -1,6 +1,6 @@
 /**
  * Created by cjoh7720 on 5/15/2017.
  */
-public class Parakeet extends Animal {
+public class Parakeet extends Animal implements Bird {
   private String parakeetCharacteristics = "Brightly colored, medium-sized parrot with long tail feathers";
 }
