91f8297fa7b5e707b3eacb49ee6ef29f8db51af4
NickCollins006 <NickCollins006@gmail.com>
2017-05-25 08:07:45 -0600

    Update GermanShephard.java

--- GermanShephard.java
+++ GermanShephard.java
@@ -2,5 +2,5 @@
  * Created by cjoh7720 on 5/15/2017.
  */
 public class GermanShephard extends Animal implements Dog {
-  public String gerShepCharacteristics = "Medium to large-sized working dog that originated in Germany.";
+  private String gerShepCharacteristics = "Medium to large-sized working dog that originated in Germany.";
 }
