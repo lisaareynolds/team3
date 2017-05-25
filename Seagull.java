efb3c01bccf86dfa584a87a13367593a1a74f96d
NickCollins006 <NickCollins006@gmail.com>
2017-05-25 08:30:45 -0600

    Update Seagull.java

--- Seagull.java
+++ Seagull.java
@@ -1,6 +1,6 @@
 /**
  * Created by cjoh7720 on 5/15/2017.
  */
-public class Seagull extends Animal {
+public class Seagull extends Animal implements Bird {
   private String seagullCharacteristics = "Medium-sized birds with primarily white feathers and distinct yellow beaks.";
 }
