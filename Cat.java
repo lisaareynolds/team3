111cc545032c57af10a36e114616dd568cc27ef8
NickCollins006 <NickCollins006@gmail.com>
2017-05-25 08:26:25 -0600

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,3 +1,3 @@
-public interface Cat{
-private static final String CHARACTERISTICS = "";
+public interface Cat {
+private static final String CHARACTERISTICS = "Four legged mammals from the felidae family";
 }
