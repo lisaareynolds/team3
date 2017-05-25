791ee3fa078c80b8849ea37a25104e711a8f7871
NickCollins006 <NickCollins006@gmail.com>
2017-05-25 08:09:11 -0600

    Update Pigeon.java

--- Pigeon.java
+++ Pigeon.java
@@ -2,4 +2,5 @@
  * Created by cjoh7720 on 5/15/2017.
  */
 public class Pigeon extends Animal {
+  private String pigeonCharacteristics = "Stout-bodied birds, with short necks and short slender beaks.";
 }
