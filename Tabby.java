124da7bc089ba8119803f8d551ded0f8d867e2d4
NickCollins006 <NickCollins006@gmail.com>
2017-05-25 08:27:29 -0600

    Update Tabby.java

--- Tabby.java
+++ Tabby.java
@@ -1,6 +1,6 @@
 /**
  * Created by cjoh7720 on 5/15/2017.
  */
-public class Tabby extends Animal {
+public class Tabby extends Animal implements Cat {
   private String tabbyCharacteristics = "Domestic cat that has a coat featuring distinctive stripes, dots, lines or swirling patterns, usually together with a mark resembling an 'M' on its forehead.";
 }
