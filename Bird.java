e3508f5d78547a3bddd8d333401c2b5ccedc12cf
NickCollins006 <NickCollins006@gmail.com>
2017-05-25 08:29:40 -0600

    Create Bird.java

---
+++ Bird.java
@@ -0,0 +1,3 @@
+public interface Bird {
+private static final String CHARACTERISTICS = "Animals grouped under the phylum chordata; feathered, beaked and talented, most having the ability to fly.";
+}
